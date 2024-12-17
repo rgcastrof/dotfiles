f6aba08e86e5815110ce58eb696284601bca09cd
rgcastrof <rogeriogirao1@proton.me>
2024-12-17 16:14:08 -0300

    update on wifi status

--- dwm/.dwmblocks/SSID.sh
+++ dwm/.dwmblocks/SSID.sh
@@ -3,8 +3,17 @@
 ssid=$(nmcli -t -f active,ssid dev wifi | grep '^sim' | cut -d':' -f2)
 
 if [ -z "$ssid" ]; then
-    echo "󰖪"
+    echo "󰤭"
 else
-#  echo "${ssid}"
-    echo "󰖩"
+    signalStrength=$(nmcli -t -f SSID,SIGNAL device wifi | grep "$ssid" | cut -d ':' -f2)
+    if [[ $signalStrength -le 100 && $signalStrength -gt 70 ]]; then
+        echo "󰤨"
+    elif [[ $signalStrength -le 70 && $signalStrength -gt 50 ]]; then
+        echo "󰤥"
+    elif [[ $signalStrength -le 50 && $signalStrength -gt 30 ]]; then
+        echo "󰤢"
+    elif [[ $signalStrength -le 30 && $signalStrength -gt 0 ]]; then
+        echo "󰤟"
+    fi
+
 fi
