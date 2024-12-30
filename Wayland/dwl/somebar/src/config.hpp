836c9dfb61b801e0e0edef9a6116cf8a989c7824
rgcastrof <rogeriogirao1@proton.me>
2024-12-30 18:49:40 -0300

    update

--- Wayland/dwl/somebar/src/config.hpp
+++ Wayland/dwl/somebar/src/config.hpp
@@ -12,7 +12,7 @@ constexpr int paddingY = 3;
 // See https://docs.gtk.org/Pango/type_func.FontDescription.from_string.html
 constexpr const char* font = "JetBrainsMonoNerdFont 11";
 
-constexpr ColorScheme colorInactive = {Color(0xbb, 0xbb, 0xbb), Color(0x00, 0x00, 0x00)};
+constexpr ColorScheme colorInactive = {Color(0xff, 0xff, 0xff), Color(0x1B, 0x1B, 0x1B)};
 constexpr ColorScheme colorActive = {Color(0x1B, 0x1B, 0x1B), Color(0xFF, 0xAD, 0x00)};
 constexpr const char* termcmd[] = {"foot", nullptr};
 
