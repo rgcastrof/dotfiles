1573c0f2c61ef1e1e433970d7dddd729da56ac88
rgcastrof <rogeriogirao1@proton.me>
2024-12-30 17:41:02 -0300

    update

--- dwm/.dwmblocks/volume.sh
+++ dwm/.dwmblocks/volume.sh
@@ -3,14 +3,14 @@
 volume=$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | tr -d '%')
 
 if [[ $volume -gt 100 ]]; then
-    echo " VOLUME ALTO"
+    echo "VOLUME ALTO"
 elif [[ $volume -gt 70 && $volume -le 100 ]]; then
-    echo "  "
+    echo " "
 elif [[ $volume -le 70 && $volume -gt 40 ]]; then
-    echo "  "
+    echo ""
 elif [[ $volume -le 40 && $volume -gt 0 ]]; then
-    echo "  "
+    echo ""
 elif [[ $volume -eq 0 ]]; then
-    echo "  "
+    echo " "
 fi
 
