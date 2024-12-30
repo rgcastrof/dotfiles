0e065d561c04835e4b89b86d27be3ffffc252498
Rogério Girão <rogeriogirao1@proton.me>
2024-12-30 18:58:37 +0000

    Add files via upload

---
+++ Wayland/dwl/somebar/src/config.hpp
@@ -0,0 +1,26 @@
+// somebar - dwl bar
+// See LICENSE file for copyright and license details.
+
+#pragma once
+#include "common.hpp"
+
+constexpr bool topbar = true;
+
+constexpr int paddingX = 10;
+constexpr int paddingY = 3;
+
+// See https://docs.gtk.org/Pango/type_func.FontDescription.from_string.html
+constexpr const char* font = "JetBrainsMonoNerdFont 11";
+
+constexpr ColorScheme colorInactive = {Color(0xbb, 0xbb, 0xbb), Color(0x00, 0x00, 0x00)};
+constexpr ColorScheme colorActive = {Color(0x1B, 0x1B, 0x1B), Color(0xFF, 0xAD, 0x00)};
+constexpr const char* termcmd[] = {"foot", nullptr};
+
+static std::vector<std::string> tagNames = {
+	"󰣇", "󰈹", " ",
+	"", " ",
+};
+
+constexpr Button buttons[] = {
+	{ ClkStatusText,   BTN_RIGHT,  spawn,      {.v = termcmd} },
+};
