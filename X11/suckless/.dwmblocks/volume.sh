fd706ec5451ec46e879e00b9b48ffeb35540376a
rgcastrof <rogeriogirao1@proton.me>
2025-01-10 20:12:43 -0300

    update volume script to add headphone icon and remove brightness script

--- X11/suckless/.dwmblocks/volume.sh
+++ X11/suckless/.dwmblocks/volume.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
+audio_output=$(pacmd list-sinks | grep active | awk '{print $3}')
 volume=$(pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}' | tr -d '%')
 
-if [[ $volume -gt 100 ]]; then
-    echo "VOLUME ALTO"
-elif [[ $volume -gt 70 && $volume -le 100 ]]; then
-    echo " "
-elif [[ $volume -le 70 && $volume -gt 40 ]]; then
-    echo ""
-elif [[ $volume -le 40 && $volume -gt 0 ]]; then
-    echo ""
-elif [[ $volume -eq 0 ]]; then
-    echo " "
+if [[ $audio_output == "<analog-output-headphones>" ]]; then
+    if [[ $volume -ge 50 ]]; then
+        echo "HIGH VOLUME"
+    else
+        echo ""
+    fi
+elif [[ $audio_output == "<analog-output-speaker>" ]]; then
+    if [[ $volume -gt 100 ]]; then
+        echo "HIGH VOLUME"
+    elif [[ $volume -gt 70 && $volume -le 100 ]]; then
+        echo " "
+    elif [[ $volume -le 70 && $volume -gt 40 ]]; then
+        echo ""
+    elif [[ $volume -le 40 && $volume -gt 0 ]]; then
+        echo ""
+    elif [[ $volume -eq 0 ]]; then
+        echo " "
+    fi
 fi
-
