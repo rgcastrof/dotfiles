10dd2c0a43a3be90e86577a170d6bac4f4de0988
rgcastrof <rogeriogirao1@proton.me>
2025-01-12 12:24:24 -0300

    create themes directory

---
+++ X11/suckless/dwm/themes/pop-dark.h
@@ -0,0 +1,5 @@
+static const char col_gray1[]       = "#1B1B1B";
+static const char col_gray2[]       = "#FFFFFF";
+static const char col_gray3[]       = "#FFAD00";
+static const char col_gray4[]       = "#0F0F0F";
+static const char col_cyan[]        = "#283747";
