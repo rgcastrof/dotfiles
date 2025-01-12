d76c33dde309314a64762da4b177b58dd12889c8
rgcastrof <rogeriogirao1@proton.me>
2025-01-12 12:37:14 -0300

    create themes directory

---
+++ X11/suckless/dmenu/themes/dark-blue.h
@@ -0,0 +1,4 @@
+static const char foreground_norm[] = "#FFFFFF";
+static const char background_norm[] = "#1B1B1B";
+static const char foreground_sel[]  = "#1B1B1B";
+static const char background_sel[]  = "#4C8CCF";
