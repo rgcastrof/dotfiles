9fc12f14a5a0a3300d3863ed265ea5489bfe1c11
Rogerio Girao <rogerio@void.localdomain>
2025-02-08 10:39:01 -0300

    switched wifi status on statubar for icons

--- X11/suckless/.dwmblocks/SSID.sh
+++ X11/suckless/.dwmblocks/SSID.sh
@@ -3,7 +3,16 @@
 bssid=$(nmcli -t -f IN-USE,BSSID device wifi | grep '*' | tr -d '\\' | cut -d ':' -f2,3,4,5,6,7)
 
 if [ -z "$bssid" ]; then
-    echo "wifi: disconnected"
+    echo "󰤭"
 else
-    echo "wifi: connected"
+    signalStrength=$(nmcli -t -f BSSID,SIGNAL device wifi | tr -d '\\' | grep "$bssid" | cut -d ':' -f7)
+    if [[ $signalStrength -le 100 && $signalStrength -gt 70 ]]; then
+        echo "󰤨"
+    elif [[ $signalStrength -le 70 && $signalStrength -gt 50 ]]; then
+        echo "󰤥"
+    elif [[ $signalStrength -le 50 && $signalStrength -gt 30 ]]; then
+        echo "󰤢"
+    elif [[ $signalStrength -le 30 && $signalStrength -gt 0 ]]; then
+        echo "󰤟"
+    fi
 fi
