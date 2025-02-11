b9416fb8dbebe651220e1d775a1e1275c1698692
rgcastrof <rogeriogirao1@proton.me>
2025-02-11 10:37:26 -0300

    update

---
+++ X11/suckless/dmenu/themes/mist-forest.h
@@ -0,0 +1,4 @@
+static const char foreground_norm[] = "#FFFFFF";
+static const char background_norm[] = "#1A2420";
+static const char foreground_sel[]  = "#2E3D30";
+static const char background_sel[]  = "#8FA07C";
