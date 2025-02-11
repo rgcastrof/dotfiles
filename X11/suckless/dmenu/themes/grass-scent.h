f2f6813c4bffef0b827b97b191a485cabf39f313
rgcastrof <rogeriogirao1@proton.me>
2025-02-11 10:15:19 -0300

    new theme added

---
+++ X11/suckless/dmenu/themes/grass-scent.h
@@ -0,0 +1,4 @@
+static const char foreground_norm[] = "#FFFFFF";
+static const char background_norm[] = "#5A4632";
+static const char foreground_sel[]  = "#5A4632";
+static const char background_sel[]  = "#DBC27D";
