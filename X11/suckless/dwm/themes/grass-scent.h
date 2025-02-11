8e23fa28c6a2335c80321dee8756c464767bddab
rgcastrof <rogeriogirao1@proton.me>
2025-02-11 10:04:06 -0300

    new themes added

---
+++ X11/suckless/dwm/themes/grass-scent.h
@@ -0,0 +1,7 @@
+static const char col_gray1[]       = "#5A4632";
+static const char col_gray2[]       = "#F8E7C1";
+static const char col_gray3[]       = "#5A4632";
+static const char col_gray4[]       = "#DBC27D";
+static const char col_gray5[]       = "#A3C970";
+static const char col_cyan[]        = "#5A4632";
+
