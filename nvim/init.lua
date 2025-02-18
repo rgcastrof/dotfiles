88ed1ef012535b54f65620f0d6b53a30d1657278
rgcastrof <rogeriogirao1@proton.me>
2025-02-17 21:14:53 -0300

    corrected error with tab autocompletion

--- nvim/init.lua
+++ nvim/init.lua
@@ -238,38 +238,21 @@ local cmp = require('cmp')
 cmp.setup({
   sources = {
     {name = 'nvim_lsp'},
-    {name = 'luasnip'}, -- Certifique-se de incluir os snippets aqui
   },
   mapping = cmp.mapping.preset.insert({
-    -- Tab para navegar entre itens e, se uma sugestão estiver visível, completar automaticamente
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item({behavior = cmp.SelectBehavior.Select})
-        -- Completar automaticamente o primeiro item
-        cmp.confirm({select = true})
-      elseif require('luasnip').expand_or_jumpable() then
-        require('luasnip').expand_or_jump()
-      else
-        fallback()
-      end
-    end, {'i', 's'}),
-
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item({behavior = cmp.SelectBehavior.Select})
-      elseif require('luasnip').jumpable(-1) then
-        require('luasnip').jump(-1)
-      else
-        fallback()
-      end
-    end, {'i', 's'}),
-
-    -- Enter para confirmar seleção
-    ['<CR>'] = cmp.mapping.confirm({select = true}),
+    ['<C-p>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+    ['<C-n>'] = cmp.mapping.select_next_item({behavior = 'select'}),
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+    -- Scroll up and down in the completion documentation
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
   }),
   snippet = {
     expand = function(args)
-      require('luasnip').lsp_expand(args.body)
+        vim.snippet.expand(args.body)
     end,
   },
 })
