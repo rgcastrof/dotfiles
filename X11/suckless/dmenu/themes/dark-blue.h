acf9d1160752653faecd59c814282e73dfddc1f3
rgcastrof <rogeriogirao1@proton.me>
2025-02-23 13:49:01 -0300

    new blue color added to dark-blue theme

--- X11/suckless/dmenu/themes/dark-blue.h
+++ X11/suckless/dmenu/themes/dark-blue.h
@@ -1,4 +1,7 @@
 static const char foreground_norm[] = "#FFFFFF";
 static const char background_norm[] = "#1B1B1B";
 static const char foreground_sel[]  = "#1B1B1B";
-static const char background_sel[]  = "#4C8CCF";
+static const char background_sel[]  = "#90909C";
+
+// iced blue color: #90909C
+// light blue color: #4C8CCF
