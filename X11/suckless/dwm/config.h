55b0430b0b73681ee8eb17f5c4e737ea43a910b4
rgcastrof <rogeriogirao1@proton.me>
2025-02-23 13:48:27 -0300

    new blue color added to dark-blue theme

--- X11/suckless/dwm/config.h
+++ X11/suckless/dwm/config.h
@@ -9,8 +9,6 @@ static const unsigned int gappx     = 7;
 static const unsigned int snap      = 15;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int splitstatus        = 1;        /* 1 for split status items */
-static const char *splitdelim        = ";";       /* Character used for separating status */
 static const char *fonts[]          = { "JetBrainsMonoNerdFont:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 
