1e11afb9c018d728bf85136d7cf9285f641da84e
rgcastrof <rogeriogirao1@proton.me>
2025-04-12 11:53:31 -0300

    boostrap script  test 3

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,7 +20,9 @@ configurar_networkmanager() {
     sudo ln -s /etc/sv/NetworkManager/ /var/service/
     sleep 10
 
-    if [[ sudo sv status NetworkManager | awk '{print $6}' == "run:" && sudo sv status dbus | awk '{print $6}' == "run:" ]]; then
+    nm_status=$(sudo sv status NetworkManager | awk '{print $6}')
+    dbus_status=$(sudo sv status dbus | awk '{print $6}')
+    if [[ "$nm_status" == "run:" && "$dbus_status" == "run:" ]]; then
         echo "NetworkManager configurado."
     else
         echo "Erro ao configurar NetworkManager"
@@ -40,10 +42,13 @@ configurar_networkmanager() {
 }
 
 configurar_tlp() {
+    echo "Ativando tlp..."
     sudo ln -s /etc/sv/tlp/ /var/service/
+    echo "tlp ativo"
 }
 
 configurar_intel_microcode() {
+    echo "Regenerando initramfs..."
     sudo xbps-reconfigure --force $(xbps-query -l | awk '{ print $2 }' | xargs -n1 xbps-uhelper getpkgname | grep "linux6")
 }
 
@@ -51,14 +56,24 @@ configurar_apparmor() {
     echo "configurando apparmor..."
 
     sudo sed -i 's/^GRUB_CMDLINE_LINUX_DEFAULT=.*/GRUB_CMDLINE_LINUX_DEFAULT="loglevel=4 apparmor=1 security=apparmor"/' /etc/default/grub
-    update-grub
+    sudo update-grub
+
+    echo "apparmor configurado"
 }
 
 configurar_interface() {
+
+    echo "Atualizando arquivos .bashrc e .bash_profile..."
     rm $HOME/.bashrc $HOME/.bash_profile
     cp $HOME/dotfiles/.bashrc $HOME/
     cp $HOME/dotfiles/.bash_profile $HOME/
 
+    echo "Copiando arquivos de configuração do teclado e touchpad para o xorg..."
+    sudo mkdir -p /etc/X11/xorg.conf.d/
+    sudo cp $HOME/dotfiles/X11/30-touchpad.conf /etc/X11/xorg.conf.d/
+    sudo cp $HOME/dotfiles/X11/10-keyboard.conf /etc/X11/xorg.conf.d/
+
+    echo "Copiando código fonte do dwm para .config..."
     mkdir -p $HOME/.config/
     cp -r $HOME/dotfiles/X11/suckless/.dwm/ $HOME/
     cp -r $HOME/dotfiles/X11/suckless/.dwmblocks/ $HOME/
@@ -66,10 +81,8 @@ configurar_interface() {
     cp -r $HOME/dotfiles/X11/suckless/dmenu/ $HOME/.config/
     cp -r $HOME/dotfiles/X11/suckless/st/ $HOME/.config/
 
-    sudo mkdir -p /etc/X11/xorg.conf.d/
-    sudo cp $HOME/dotfiles/X11/30-touchpad.conf /etc/X11/xorg.conf.d/
-    sudo cp $HOME/dotfiles/X11/10-keyboard.conf /etc/X11/xorg.conf.d/
 
+    echo "Compilando dwm..."
     cd $HOME/.config/dwm/
     make && sudo make install
     cd $HOME/.config/st/
@@ -82,14 +95,18 @@ configurar_interface() {
 }
 
 configurar_permissoes() {
+
+    echo "Atualizando permissões do diretório ~/.cache..."
     mkdir -p $HOME/.cache/
     chown -R $USER:$USER ~/.cache/
     chmod -R u+rw ~/.cache/
+    echo "Atualizando permissões do brilho da tela..."
     sudo mkdir -p /etc/udev/rules.d/
     sudo echo SUBSYSTEM=="backlight", ACTION=="add", KERNEL=="intel_backlight", GROUP="video", MODE="0660" > /etc/udev/rules.d/90-backlight.rules
 }
 
 configurar_slim() {
+    echo "Configurando o display manager slim"
     sudo rm -f /etc/slim.conf
     sudo cp $HOME/dotfiles/X11/slim/slim.conf /etc/slim/
     sudo cp -rf $HOME/dotfiles/X11/slim/bluer/ /usr/share/slim/themes/
@@ -97,6 +114,7 @@ configurar_slim() {
 }
 
 configurar_fontes() {
+    echo "Instalando fontes..."
     wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/Hack.zip
     wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/JetBrainsMono.zip
 
@@ -113,6 +131,7 @@ configurar_fontes() {
 }
 
 configurar_flatpak() {
+    echo "Adicionando o repositório flathub..."
     sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 }
 
@@ -124,6 +143,8 @@ configurar_interface
 configurar_permissoes
 configurar_fontes
 configurar_flatpak
+
+echo "Pós-instalação concluída"
 
 read -rp "Reiniciar agora? [s/N] " resposta
 if [[ "$resposta" =~ ^[sS]$ ]]; then
