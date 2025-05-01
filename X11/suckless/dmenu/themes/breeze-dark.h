92cb16164fef6cc32705163a9a5f090c5db50729
rgcastrof <rogeriogirao1@proton.me>
2025-05-01 07:29:41 -0300

    breeze colorscheme and date patch

---
+++ X11/suckless/dmenu/themes/breeze-dark.h
@@ -0,0 +1,7 @@
+static const char foreground_norm[] = "#eff0f1";
+static const char background_norm[] = "#31363b";
+static const char foreground_sel[]  = "#1B1B1B";
+static const char background_sel[]  = "#3daee9";
+
+// iced blue color: #90909C
+// light blue color: #4C8CCF
