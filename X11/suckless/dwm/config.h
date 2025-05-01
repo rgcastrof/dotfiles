9fcf7eee55ef5f110d6c66d88caa3290a53049d0
rgcastrof <rogeriogirao1@proton.me>
2025-05-01 11:10:56 -0300

    updated icon

--- X11/suckless/dwm/config.h
+++ X11/suckless/dwm/config.h
@@ -45,7 +45,7 @@ static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ " ",      tile },    /* first entry is default */
 	{ " ",      NULL },    /* no layout function means floating behavior */
-	{ " ",      monocle },
+	{ " ",      monocle },
 };
 
 /* key definitions */
