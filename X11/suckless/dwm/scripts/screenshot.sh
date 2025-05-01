95cfeba062b37e74c2692ecda55edc9cdf2e41eb
rgcastrof <rogeriogirao1@proton.me>
2025-05-01 09:57:12 -0300

    updated screenshot script

--- X11/suckless/dwm/scripts/screenshot.sh
+++ X11/suckless/dwm/scripts/screenshot.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-/usr/bin/scrot -s /home/owl/Imagens/Screenshots/screenshot_%Y-%m-%d_%H-%M-%S.png
+current=$(date +%Y-%m-%d_%H-%M-%S).png
+
+/usr/bin/scrot -s /home/owl/Imagens/Screenshots/screenshot_$current
+
+notify-send "Screenshot ${current} taken successfully!"
