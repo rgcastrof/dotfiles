8924fb7e2adfc65819ce9e24be59d6ced4aeef6b
rgcastrof <rogeriogirao1@proton.me>
2025-06-09 23:02:41 -0300

    update path & set gaps

--- X11/suckless/.dwm/autostart.sh
+++ X11/suckless/.dwm/autostart.sh
@@ -2,5 +2,5 @@
 
 dwmblocks &
 dunst &
-feh --bg-scale /home/falcon/Imagens/Wallpapers/forest-clearing.jpeg
+feh --bg-scale /home/goku/Imagens/Wallpapers/forest-clearing.jpeg
 xautolock -time 10 -locker slock
--- X11/suckless/dwm/scripts/screenshot.sh
+++ X11/suckless/dwm/scripts/screenshot.sh
@@ -2,6 +2,6 @@
 
 current=$(date +%Y-%m-%d_%H-%M-%S).png
 
-/usr/bin/scrot -s /home/falcon/Imagens/Screenshots/screenshot_$current
+/usr/bin/scrot -s /home/goku/Imagens/Screenshots/screenshot_$current
 
 notify-send "Screenshot ${current} taken successfully!"
