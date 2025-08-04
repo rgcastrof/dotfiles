39cb16474e0d030691a5f4e5b19e431f7ad4dcbc
rgcastrof <rogeriogirao1@proton.me>
2025-08-04 11:08:01 -0300

    alacritty colorscheme

---
+++ X11/suckless/st/themes/alacritty.h
@@ -0,0 +1,20 @@
+static const char black[]   = "#1d1f21";
+static const char red[]     = "#cc6666";
+static const char green[]   = "#b5bd68";
+static const char yellow[]  = "#f0c674";
+static const char blue[]    = "#81a2be";
+static const char magenta[] = "#b294bb";
+static const char cyan[]    = "#8abeb7";
+static const char white[]   = "#c5c8c6";
+
+static const char bright_black[]   = "#666666";
+static const char bright_red[]     = "#d54e53";
+static const char bright_green[]   = "#b9ca4a";
+static const char bright_yellow[]  = "#e7c547";
+static const char bright_blue[]    = "#7aa6da";
+static const char bright_magenta[] = "#c397d8";
+static const char bright_cyan[]    = "#70c0b1";
+static const char bright_white[]   = "#eaeaea";
+
+static const char foreground[]     = "#e0def4";
+static const char background[]     = "#0f0f0f";
