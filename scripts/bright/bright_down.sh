cf535c55f8631a2ebae1d6d5d7e8d9ff6388b556
rgcastrof <rogeriogirao1@proton.me>
2025-08-27 19:33:45 -0300

    rewrite bright scripts

--- scripts/bright/bright_down.sh
+++ scripts/bright/bright_down.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+current=$(cat /sys/class/backlight/intel_backlight/brightness)
+max=$(cat /sys/class/backlight/intel_backlight/max_brightness)
 
-brightnessctl set 5%-
+((incr = max * 5 / 100))
+
+(( current -= incr ))
+
+if [ $current -gt $max ]; then
+	(( current = max ))
+fi
+
+echo $current | doas tee /sys/class/backlight/intel_backlight/brightness > /dev/null
