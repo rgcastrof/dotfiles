dffef0c84a80e900f131a63eb55d857170b53817
rgcastrof <rogeriogirao1@proton.me>
2025-08-30 21:03:54 -0300

    update dmenu

--- X11/suckless/dmenu/config.def.h
+++ X11/suckless/dmenu/config.def.h
@@ -10,7 +10,7 @@ static int min_width = 500;                    /* minimum width when centered */
 static const float menu_height_ratio = 4.0f;  /* This is the ratio used in the original calculation */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"CaskaydiaCoveNerdFont:size=12"
+	"CaskaydiaCoveNerdFont:size=13"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
