ca6510bfaf743ef3325ab9e4d72741c24aae8f51
rgcastrof <rogeriogirao1@proton.me>
2025-09-05 07:44:31 -0300

    update dwm themes

--- X11/suckless/.dwmblocks/battery.sh
+++ X11/suckless/.dwmblocks/battery.sh
@@ -3,50 +3,76 @@
 battery=$(cat /sys/class/power_supply/BAT0/capacity)
 charging_status=$(cat /sys/class/power_supply/BAT0/status)
 
+# if [ "$charging_status" = "Charging" ]; then
+#     if [[ $battery -gt 95 ]]; then
+#         echo "󰂅 $battery% | "
+#     elif [[ $battery -le 95 && $battery -gt 90 ]]; then
+#         echo "󰂋 $battery% | "
+#     elif [[ $battery -le 90 && $battery -gt 80 ]]; then
+#         echo "󰂊 $battery% | "
+#     elif [[ $battery -le 80 && $battery -gt 70 ]]; then
+#         echo "󰂊 $battery% | "
+#     elif [[ $battery -le 70 && $battery -gt 60 ]]; then
+#         echo "󰢞 $battery% | "
+#     elif [[ $battery -le 60 && $battery -gt 50 ]]; then
+#         echo "󰂉 $battery% | "
+#     elif [[ $battery -le 50 && $battery -gt 40 ]]; then
+#         echo "󰢝 $battery% | "
+#     elif [[ $battery -le 40 && $battery -gt 30 ]]; then
+#         echo "󰂈 $battery% | "
+#     elif [[ $battery -le 30 && $battery -gt 20 ]]; then
+#         echo "󰂇 $battery% | "
+#     elif [[ $battery -le 20 && $battery -gt 10 ]]; then
+#         echo "󰂆 $battery% | "
+#     elif [[ $battery -le 10 ]]; then
+#         echo "󰢜 $battery% | "
+#     fi
+# else
+#     if [[ $battery -gt 90 ]]; then
+#         echo "󰁹 $battery% | "
+#     elif [[ $battery -le 90 && $battery -gt 80 ]]; then
+#         echo "󰂂 $battery% | "
+#     elif [[ $battery -le 80 && $battery -gt 70 ]]; then
+#         echo "󰂁 $battery% | "
+#     elif [[ $battery -le 70 && $battery -gt 60 ]]; then
+#         echo "󰂀 $battery% | "
+#     elif [[ $battery -le 60 && $battery -gt 50 ]]; then
+#         echo "󰁿 $battery% | "
+#     elif [[ $battery -le 50 && $battery -gt 40 ]]; then
+#         echo "󰁾 $battery% | "
+#     elif [[ $battery -le 40 && $battery -gt 30 ]]; then
+#         echo "󰁽 $battery% | "
+#     elif [[ $battery -le 30 && $battery -gt 20 ]]; then
+#         echo "󰁼 $battery% | "
+#     elif [[ $battery -le 20 && $battery -gt 10 ]]; then
+#         echo "󰁻 $battery% | "
+#     elif [[ $battery -le 10 ]]; then
+#         echo "󰁺 $battery% | "
+#     fi
+# fi
+
 if [ "$charging_status" = "Charging" ]; then
-    if [[ $battery -gt 95 ]]; then
-        echo "󰂅 $battery% | "
-    elif [[ $battery -le 95 && $battery -gt 90 ]]; then
-        echo "󰂋 $battery% | "
-    elif [[ $battery -le 90 && $battery -gt 80 ]]; then
-        echo "󰂊 $battery% | "
-    elif [[ $battery -le 80 && $battery -gt 70 ]]; then
-        echo "󰂊 $battery% | "
-    elif [[ $battery -le 70 && $battery -gt 60 ]]; then
-        echo "󰢞 $battery% | "
-    elif [[ $battery -le 60 && $battery -gt 50 ]]; then
-        echo "󰂉 $battery% | "
-    elif [[ $battery -le 50 && $battery -gt 40 ]]; then
-        echo "󰢝 $battery% | "
-    elif [[ $battery -le 40 && $battery -gt 30 ]]; then
-        echo "󰂈 $battery% | "
-    elif [[ $battery -le 30 && $battery -gt 20 ]]; then
-        echo "󰂇 $battery% | "
-    elif [[ $battery -le 20 && $battery -gt 10 ]]; then
-        echo "󰂆 $battery% | "
-    elif [[ $battery -le 10 ]]; then
-        echo "󰢜 $battery% | "
+    if [[ $battery -gt 80 ]]; then
+        echo " 󱐋$battery% | "
+    elif [[ $battery -le 80 && $battery -gt 60 ]]; then
+        echo " 󱐋$battery% | "
+    elif [[ $battery -le 60 && $battery -gt 40 ]]; then
+        echo " 󱐋$battery% | "
+    elif [[ $battery -le 40 && $battery -gt 20 ]]; then
+        echo " 󱐋$battery% | "
+    elif [[ $battery -le 20 ]]; then
+        echo " 󱐋$battery% | "
     fi
 else
-    if [[ $battery -gt 90 ]]; then
-        echo "󰁹 $battery% | "
-    elif [[ $battery -le 90 && $battery -gt 80 ]]; then
-        echo "󰂂 $battery% | "
-    elif [[ $battery -le 80 && $battery -gt 70 ]]; then
-        echo "󰂁 $battery% | "
-    elif [[ $battery -le 70 && $battery -gt 60 ]]; then
-        echo "󰂀 $battery% | "
-    elif [[ $battery -le 60 && $battery -gt 50 ]]; then
-        echo "󰁿 $battery% | "
-    elif [[ $battery -le 50 && $battery -gt 40 ]]; then
-        echo "󰁾 $battery% | "
-    elif [[ $battery -le 40 && $battery -gt 30 ]]; then
-        echo "󰁽 $battery% | "
-    elif [[ $battery -le 30 && $battery -gt 20 ]]; then
-        echo "󰁼 $battery% | "
-    elif [[ $battery -le 20 && $battery -gt 10 ]]; then
-        echo "󰁻 $battery% | "
-    elif [[ $battery -le 10 ]]; then
-        echo "󰁺 $battery% | "
+    if [[ $battery -gt 80 ]]; then
+        echo " $battery% | "
+    elif [[ $battery -le 80 && $battery -gt 60 ]]; then
+        echo " $battery% | "
+    elif [[ $battery -le 60 && $battery -gt 40 ]]; then
+        echo " $battery% | "
+    elif [[ $battery -le 40 && $battery -gt 20 ]]; then
+        echo " $battery% | "
+    elif [[ $battery -le 20 ]]; then
+        echo " $battery% | "
     fi
 fi
---
+++ X11/suckless/dmenu/themes/void.h
@@ -0,0 +1,5 @@
+static const char foreground_norm[] = "#f5f1e6";
+static const char background_norm[] = "#4d4d4d";
+static const char foreground_sel[]  = "#4d4d4d";
+static const char background_sel[]  = "#b8c94a";
+
