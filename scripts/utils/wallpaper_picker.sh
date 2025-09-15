754c4d6137fc4628cb52f1fd73ec812ac157bd13
rgcastrof <rogeriogirao1@proton.me>
2025-09-15 14:47:27 -0300

    update wallpaper script

--- scripts/utils/wallpaper_picker.sh
+++ scripts/utils/wallpaper_picker.sh
@@ -5,6 +5,6 @@ if [ -z "$wallpaper" ]; then
     notify-send "Nothing was selected"
 else
     feh --bg-scale "$HOME/Pictures/Wallpapers/$wallpaper"
-    sed -i "6s|[^/]*$|$wallpaper|" "$HOME/.dwm/autostart.sh"
+    sed -i "6s|[^/]*$|$wallpaper|" "$HOME/.config/suckless/dwm/autostart.sh"
     echo "wallpaper: $wallpaper set"
 fi
