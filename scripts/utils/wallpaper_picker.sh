3aeff0841040466554256ce6304a4b8ec939365d
rgcastrof <rogeriogirao1@proton.me>
2025-09-30 20:38:18 -0300

    correct wallpaper set notification

--- scripts/utils/wallpaper_picker.sh
+++ scripts/utils/wallpaper_picker.sh
@@ -2,9 +2,9 @@
 wallpaper=$(ls "$HOME/Pictures/Wallpapers" | dmenu -l 7 -p "select wallpaper:")
 
 if [ -z "$wallpaper" ]; then
-    notify-send "Nothing was selected"
+    dunstify -a "Wallpaper" -r 9993 -u low "Nothing was selected"
 else
     feh --bg-scale "$HOME/Pictures/Wallpapers/$wallpaper"
     sed -i "6s|[^/]*$|$wallpaper|" "$HOME/.config/suckless/dwm/autostart.sh"
-    echo "wallpaper: $wallpaper set"
+    dunstify -a "Wallpaper" -r 9993 -u normal "wallpaper: $wallpaper set"
 fi
