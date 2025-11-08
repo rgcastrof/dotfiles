a9c04c2f971e0a87ff0bed46b8dedbc52885d26a
rgcastrof <rogeriogirao1@proton.me>
2025-11-08 17:46:14 -0300

    update volume scripts

--- scripts/volume/mute.sh
+++ scripts/volume/mute.sh
@@ -2,7 +2,7 @@
 
 source "$HOME/.config/scripts/volume/volume_utils.sh"
 
-pactl set-sink-volume @DEFAULT_SINK@ -5%
+pactl set-sink-mute @DEFAULT_SINK@ toggle
 
 vol=$(get_volume)
 muted=$(is_muted && echo "yes" || echo "no")
