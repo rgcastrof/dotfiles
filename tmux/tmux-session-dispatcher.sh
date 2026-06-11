2d8446cdf99086226bc641a96a09a2ca0cb5028b
rgcastrof <rogeriogirao1@proton.me>
2026-06-11 20:39:44 -0300

    update script and bar

--- tmux/tmux-session-dispatcher.sh
+++ tmux/tmux-session-dispatcher.sh
@@ -4,10 +4,14 @@
 
 [ -z "$selected" ] && exit 0
 
-selected_name="$(basename "$selected")"
+selected_name="$(basename "$selected" | tr '.' '_')"
 
-if ! tmux has-session -t "$selected_name"; then
+if ! tmux has-session -t "$selected_name" 2>/dev/null; then
 	tmux new-session -ds "$selected_name" -c "$selected"
 fi
 
-tmux switch-client -t "$selected_name"
+if [ -z "$TMUX" ]; then
+	tmux attach-session -t "$selected_name"
+else
+	tmux switch-client -t "$selected_name"
+fi
